1cda5f11cb84ca8e72f265c770abf5dacd19d2bb
Renington Neri <renington.neri@gmail.com>
2017-10-22 11:03:31 -0200

    Change database and seeds

--- config/database.rb
+++ config/database.rb
@@ -20,13 +20,15 @@ configure :test do
   }
  end
  
- configure :production do
-    set :database, {
-      adapter: 'postgresql',
-      encoding: 'utf8',
-      database: 'onebitbot_production',
-      pool: 5,
-      username: 'postgres',
-      host: 'postgres'
-    }
- end
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///postgres/onebitbot_production')
+
+  set :database, {
+    adapter:  db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    host:     db.host,
+    username: db.user,
+    password: db.password,
+    database: db.path[1..-1],
+    encoding: 'utf8'
+  }
+end
